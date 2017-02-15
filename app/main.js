ce85fc9d1faf52732f22f170752d217fc4e0d514
Drew Douglass <drew@creatingdrew.com>
2017-02-15 14:09:39 -0600

    Updated paths to work on remote server as well.

--- app/main.js
+++ app/main.js
@@ -1,20 +1,20 @@
 requirejs.config({
     paths: {
-        'text': '../libs/requirejs-text/text',
-        'durandal': '../libs/Durandal/js',
-        'plugins': '../libs/Durandal/js/plugins',
-        'transitions': '../libs/Durandal/js/transitions',
-        'knockout': '../libs/knockout/dist/knockout',
-        'jquery': '../libs/jquery/dist/jquery',
-        'bootstrap': '../libs/bootstrap-sass/assets/javascripts/bootstrap',
-        'owl-carousel': '../libs/owl.carousel/dist/owl.carousel',
-        'match-height': '../libs/matchHeight/dist/jquery.matchHeight',
-        'fancybox': '../libs/fancybox/source/jquery.fancybox.pack',
-        'fancybox-media': '../libs/fancybox/source/helpers/jquery.fancybox-media',
-        'authenticate': '../services/authentication',
-        'moment': '../libs/moment/moment',
-        'crypto-js': '../libs/crypto-js/crypto-js',
-        'sweetalert': '../libs/sweetalert/dist/sweetalert.min'
+        'text': 'libs/requirejs-text/text',
+        'durandal': 'libs/Durandal/js',
+        'plugins': 'libs/Durandal/js/plugins',
+        'transitions': 'libs/Durandal/js/transitions',
+        'knockout': 'libs/knockout/dist/knockout',
+        'jquery': 'libs/jquery/dist/jquery',
+        'bootstrap': 'libs/bootstrap-sass/assets/javascripts/bootstrap',
+        'owl-carousel': 'libs/owl.carousel/dist/owl.carousel',
+        'match-height': 'libs/matchHeight/dist/jquery.matchHeight',
+        'fancybox': 'libs/fancybox/source/jquery.fancybox.pack',
+        'fancybox-media': 'libs/fancybox/source/helpers/jquery.fancybox-media',
+        'authenticate': 'services/authentication',
+        'moment': 'libs/moment/moment',
+        'crypto-js': 'libs/crypto-js/crypto-js',
+        'sweetalert': 'libs/sweetalert/dist/sweetalert.min'
     },
   shim: {
     bootstrap: {
